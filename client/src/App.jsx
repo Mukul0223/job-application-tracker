47d3c5e8b64b9cfd5d4a39fc8ff8cc1580263289
Mukul <mukulyadav2302@gmail.com>
2026-08-21 12:10:59 +0530

    chore: configure ESLint and Prettier for client and server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import "./App.css";
+import './App.css';
 
 const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold text-blue-600">Wellcome to Jumanji!</h1>
+      <p>Well well</p>
+      <p>hello</p>
     </div>
   );
 };
